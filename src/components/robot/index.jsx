b70af2f0272ff8e4b5052bc3300ade22a8b2a3ea
inodw1 <inodwagachchi@gmail.com>
2023-11-25 22:23:07 +0530

    Update robot image

--- src/components/robot/index.jsx
+++ src/components/robot/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./robot.scss";
 
 const RobotComponent = () => {
+    const robotImageSrc = "/robot.jpg";
     return (
         <div className="robot-container">
             <span className="robot-emoji" role="img" aria-label="robot">
-                🤖
+                <img src={robotImageSrc} alt="Robot" className="robot-image" />
             </span>
         </div>
     );
