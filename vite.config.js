cacf92dcbbf88ec6553fbb76be9281c5711bc1cd
inodw1 <inodwagachchi@gmail.com>
2023-11-26 11:41:52 +0530

    Fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@import "./src/assets/styles/global.scss"; `,
+                additionalData: `@import "./src/assets/styles/global.scss";`,
             },
         },
     },
